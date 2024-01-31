d6774a6700d096ebe66854d5db62fa272038f400
Marco <patrickstar1281@yahoo.com>
2024-01-30 20:06:31 -0600

    added templates and styles to project

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "This is the home page"
+def home_page():
+    return render_template('entry.html', the_title='Welcome to the Mata Forum Page!')
 
 if __name__ == "__main__":
     app.run(debug=True)
