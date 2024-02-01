0c8d81ac5e615d31bbf8a0b7143f1d68c7c42ea6
Marco <patrickstar1281@yahoo.com>
2024-01-31 19:50:16 -0600

    adding login template and flask  connections

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home_page():
     return render_template('entry.html', the_title='Welcome to the Mata Forum Page!')
 
+@app.route('/login')
+def login_page():
+    return render_template('login.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
