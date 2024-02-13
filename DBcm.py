d68d195f085b0711a1ff12df99117c79e110e33a
Marco <patrickstar1281@yahoo.com>
2024-02-12 18:03:04 -0600

    setup for posting data to sql

--- DBcm.py
+++ DBcm.py
@@ -1,4 +1,4 @@
-import mysql.connector
+import mysql.connector, hashlib
 
 class UseDatabase:
     def __init__(self, config: dict):
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import find_dotenv, load_dotenv
-from flask import Flask, render_template, requests, session
+from flask import Flask, render_template, session, request
 from checker import check_logged_in
 
 dotenv_path = find_dotenv()
@@ -19,17 +19,22 @@ def home_page():
 def secret_page():
     return "if you are seeing this then you are logged in!"
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login_page():
-    session['logged_in'] = True
-    return "you have logged in!"
+    # error message
+    msg = ''
+
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        user
+
+    return render_template('login.html', msg='')
 
 @app.route('/logout')
 def do_logout():
     session.pop('logged_in')
     return 'You have logged out'
 
-app.secret_key = ''
+app.secret_key = os.getenv("secret_key")
 
 if __name__ == "__main__":
     app.run(debug=True)
