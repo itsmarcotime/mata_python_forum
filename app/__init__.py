7c354e73bfc47ac70c32d3b06cbeb2776576df9a
Marco <patrickstar1281@yahoo.com>
2024-03-14 22:14:18 -0500

    small config commit

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+import os
 
 def create_app():
-    pass
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.getenv("secret_key")
+
+    return app
+
