025279e7b8ef443d81fcf4ba9f45772fe1613d0f
Marco <patrickstar1281@yahoo.com>
2024-03-19 20:35:13 -0500

    having trouble with render template on app starting over

--- app/main_pages.py
+++ app/main_pages.py
@@ -4,4 +4,4 @@ main_pages = Blueprint("main_pages", __name__)
 
 @main_pages.route('/')
 def base_page():
-    return render_template('base.html', the_title='Welcome to the Mata Forum Page!')
+    return render_template('test.html')
